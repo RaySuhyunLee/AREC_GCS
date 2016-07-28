26c60fb52f02255d254e93ba4643887b41051d46
이수현 <=>
2016-07-28 17:10:05 +0900

    IPC communication

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
 
 let win
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 800, height: 600, show: false});
 
   win.loadURL(`file://${__dirname}/index.html`);
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 
   win.on('closed', () => {
     win = null;
   });
 }
 
+ipcMain.on('async-msg', (event, arg) => {
+  console.log(arg);
+  event.sender.send('async-reply', 'pong');
+});
+
+ipcMain.on('sync-msg', (event, arg) => {
+  console.log(arg);
+  event.returnValue = 'pong';
+});
+
 app.on('ready', createWindow);
-
-
