af2718c1448a6be23c1e1194659b7f180e608a91
이수현 <=>
2016-07-28 15:06:13 +0900

    Hello world example

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron');
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600});
+
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+
