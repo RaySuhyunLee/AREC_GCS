c0f782efac1cb844335aaf0ee5bd8d65124a26d3
이수현 <=>
2016-07-31 00:42:28 +0900

    sensor value data binding

--- js/index.js
+++ js/index.js
@@ -15,8 +15,82 @@ wall.reset({
 });
 wall.fitWidth();
 
+// angularjs setting
+var panelApp = angular.module('panelApp', []);
+var panelAppScope = null;
+panelApp.controller('panelController', ($scope) => {
+  panelAppScope = $scope;
+  $scope.values = values;
+});
+
+// value set for satellite data visualization 
+var values = [
+{
+  name: "10분 평균 방사능",
+  index: 10,
+  unit: "msv/h"
+},
+{
+  name: "1분 평균 방사능",
+  index: 11,
+  unit: "msv/h"
+},
+{
+  name: "위도",
+  index: 7,
+  unit: "°"
+},
+{
+  name: "경도",
+  index: 8,
+  unit: "°"
+},
+{
+  name: "고도",
+  index: 6,
+  unit: "m"
+},
+{
+  name: "Roll",
+  index: 4,
+  unit: "°"
+},
+{
+  name: "Pitch",
+  index: 3,
+  unit: "°"
+},
+{
+  name: "Yaw",
+  index: 2,
+  unit: "°"
+},
+{
+  name: "배터리 보유 전압",
+  index: 15,
+  unit: "V"
+},
+{
+  name: "태양전지 발전 전압",
+  index: 14,
+  unit: "V"
+},
+{
+  name: "기기 소모 전류",
+  index: 13,
+  unit: "A"
+},
+{
+  name: "태양 전지 발전 전류",
+  index: 12,
+  unit: "A"
+}
+];
+
 // serial port communication
 var port = null;
+var satData = [];
+var lastUpdate = null;
 
 $('#port-sync-button').on('click', (v) => {
   SerialPort.list((err, ports) => {
@@ -43,10 +117,13 @@ $('#connect-button').on('click', (v) => {
   });
   
   port.on('data', function (buf) {
-    var satData = PacketParser.parse(buf);
-    var lastUpdate = new Date();
-    console.log("recv> " + data);
-
+    satData = PacketParser.parse(buf);
+    lastUpdate = new Date();
+    console.log("recv> " + satData);
+    if (panelAppScope) {
+      panelAppScope.data = satData;
+      panelAppScope.$apply();
+    }
   });
 });
 
